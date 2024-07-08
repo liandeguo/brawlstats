fbee55e70b5172b975dcbc40cccef0adc141d42e
Liande Guo <104030726+liandeguo@users.noreply.github.com>
2024-07-08 17:41:56 +0200

    First Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,92 @@
+<div class="bg">
+    <h1>Brawl Track by Soty Ltd.</h1>
+    <div style="z-index: 100; display: {welcome};">
+        <h2>Welcome</h2>
+        <h3>Analyze your Brawl Stars Account</h3>
+        <button on:click={getstarted}>Get Started</button>
+    </div>
+
+    <div style="z-index: 100; display: {sign};">
+        <h2>Sign in</h2>
+        <h3>Enter your Brawl Stars ID</h3>
+        <input type="text" placeholder="#YOURID">
+    </div>
+</div>
+<div class="black"></div>
+<div class="shadow"></div>
+<style>
+    .bg {   
+        background-image: url(loading_2020_arcade.png);
+        width: 100vw;
+        height: 100vh;
+        background-size: cover;
+        background-position: center;
+        display: flex;
+        justify-content: space-between;
+        flex-direction: column;
+        align-items: center;
+        text-align: center;
+    }
+
+    h3 {
+        font-size: 15px;
+        font-weight: 300;
+    }
+
+    h2 {
+        font-size: 40px;
+    }
+
+    h1 {
+        font-size: 10px;
+    }
+
+    button {
+        border: 0px;
+        background-color: #DD76DF;
+        color: black;
+        border-radius: 15px;
+        padding: 15px 30px;
+        font-weight: bold;
+        font-size: 17px;
+        margin: 30px 0 30px 0 ;
+    }
+
+    input {
+        border: 0px;
+        background-color: #E8A2EA;
+        color: black;
+        border-radius: 15px;
+        padding: 15px 30px;
+        font-weight: bold;
+        font-size: 17px;
+        margin: 30px 0 30px 0 ;
+    }
+    .shadow {
+        width: 100vw;
+        height: 40vh;
+        background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,1));
+        position:absolute;
+        bottom: 15vh;
+    }
+
+    .black {
+        position: absolute;
+        background-color: #000;
+        bottom: 0;
+        height: 15vh;
+        width: 100vw;
+    }
+
+</style>
+
+<script>
+    var welcome = 'block'
+    var sign = 'none'
+
+    function getstarted() {
+        welcome = 'none'
+        sign = 'block'
+    }
+</script>
+
---
+++ src/routes/account/+page.svelte
@@ -0,0 +1,164 @@
+<script>
+    import { onMount } from 'svelte';
+    import { page } from '$app/stores'
+
+    const id = $page.url.searchParams.get('id')
+    const url = encodeURI('https://bsproxy.royaleapi.dev/v1/players/%23' + id + '?Authorization=Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6Ijk3ZDRhMzIzLWJiMmItNDYyNS04OGY3LWRhOWQ3MzUyOWZjNCIsImlhdCI6MTcyMDAyOTE1Mywic3ViIjoiZGV2ZWxvcGVyLzQ3YjJmZDgxLTZjYmEtN2QxYS0wZWIyLWJhZTIwY2FkNDNhMSIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiNDUuNzkuMjE4Ljc5Il0sInR5cGUiOiJjbGllbnQifV19.rhcA-17kcd7hdO3oGZQIMENFVMeQOOp7giJ5PvsOn5mhX7fuuuxbiggQdjonSOvJySUMBCB5UI43hn2nuQIwXg');
+    const fetchurl = 'https://api.allorigins.win/get?url=' + url;
+
+    let playerName = '';
+    let trophies = 0;
+    let highestTrophies = 0;
+    let difference = 0;
+    let duoVictories = 0;
+    let vs3Victories = 0;
+    let soloVictories = 0;
+    let brawlersLength = 0;
+    let brawlers = [];
+    let tag = '';
+    async function getJSON(url) {
+        try {
+            const response = await fetch(url);
+            if (!response.ok) { throw new Error(response.statusText); }
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            return error;
+        }
+    };
+
+    getJSON(fetchurl).then(data => {
+        data = JSON.parse(data.contents);
+        playerName = data.name;
+        trophies = data.trophies;
+        highestTrophies = data.highestTrophies;
+        difference = highestTrophies - trophies;
+        duoVictories = data.duoVictories;
+        vs3Victories = data['3vs3Victories'];
+        soloVictories = data.soloVictories;
+        brawlersLength = data.brawlers.length;
+        tag = data.tag;
+        brawlers = data.brawlers;
+        // console.log(data);
+        // console.log(brawlers)s
+        console.log(playerName);      
+        console.log(brawlers.name)  
+    });
+    
+</script>
+
+<main>
+    <div class="container">
+        <div class="card" style="background-color: #CD6441;">
+            <h2 id="name">{playerName}</h2>
+            <span><img src="trophy.png" alt="trohpy"><p>{trophies}</p></span>
+            <span style="align-items: end;"><img src="id.png" alt="brawlstars id"><p>{tag}</p></span>
+        </div>
+        <div class="card" style="background-color: #1A2F21;">
+            <div>
+                <h3>Most Trophies</h3>
+                <span><img src="trophy.png" alt="trohpy"><p>{highestTrophies}</p></span>
+                <p><xxx>+{difference}</xxx> more than<br>current Trophies</p>
+            </div>
+        </div>
+    </div>
+<div class="container"> 
+        <div class="card" style="background-color: #426F3F;">
+            <div>
+                <h3>Wins</h3>
+                <span><img src="solo.png" alt="Solo Victories">{soloVictories}</span>
+                <span><img src="duo.png" alt="Duo Victories">{duoVictories}</span>
+                <span><img src="3v3.png" alt="3v3 Victories">{vs3Victories}</span>
+            </div>
+        </div>
+        <div class="card" style="background-color: #E8C05E; text-align:center" >
+            <p style="font-size: 24px;">Has <b>{brawlersLength}</b> from<br>80 Brawlers</p>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="card" style="background-color: #6f3f61;">
+            <h3>Potential Trophies</h3>
+            <p>Potential Trophies show how many Trophies you could have if they were all on</p>
+            <p>Rank 20 {brawlersLength* 500}</p>
+            <p>Rank 25 {brawlersLength* 750}</p>
+        </div>
+        <div class="card" style="background-color: #1A2F21;">
+            <div>
+                <h3>Most Trophies</h3>
+                <span><img src="trophy.png" alt="trohpy"><p>{highestTrophies}</p></span>
+                <p><xxx>+{difference}</xxx> more than<br>current Trophies</p>
+            </div>
+        </div>
+    </div>
+    <div style="display: flex;">
+        {#each brawlers as brawler}
+            <div class="card" style="background: url(portrait/{brawler.name.toLowerCase()}_portrait.png); background-size: cover;">
+                <h1>{brawler.name.toLowerCase()}</h1>
+            </div> 
+        {/each}
+    </div>
+</main>
+<style>
+    /* .grid-container {
+        display: grid; 
+        grid-template-columns: 1fr 1fr; 
+        grid-template-rows: 1fr 1fr; 
+        gap: 50px 50px; 
+        grid-template-areas: 
+            ". ."
+            ". ."; 
+    }
+
+    .grid-item {
+        border-radius: 20px;
+        width: 200px;
+        height: 200px;
+    } */
+
+    main {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+    }
+
+    #name {
+        font-size: 28px;
+    }
+
+     .container {
+        display: flex;
+     }
+
+     .card {
+        border-radius: 15px;
+        width: 40vw;
+        height: 40vw;
+        margin: 5px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+     }
+
+     span { 
+        display: flex;
+        align-items: center;
+     }
+
+     img {
+        height: 25px;
+        margin: 0 2px 0 0;
+     }
+     xxx {
+        color: #7EB500;
+     }
+     @media screen and (min-width: 500px){
+        .card {
+            width: 200px;
+            height: 200px;
+        }
+    }
+
+</style>
